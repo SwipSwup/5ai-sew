c1d1f45b526e518470ab7e6f9fff273b5b5a3c7b
F. Kasper <ksp@htl.rennweg.at>
2021-10-13 09:24:36 +0200

    Client: SongView-Komponente einfach implementieren

--- client/src/components/SongView.vue
+++ client/src/components/SongView.vue
@@ -1,16 +1,43 @@
 <template>
   <div>
-    <song :song="{ title: 'Foo', artist: 'Bar', genre: 'Baz' }" />
+    <song
+        v-for="s in songs"
+        :key="s._links.self.href"
+        :song="s"
+    />
   </div>
 </template>
 
 <script>
 import Song from '@/components/Song'
+import axios from 'axios'
 
 export default {
     name: 'SongView',
+
     components: {
         Song,
+    },
+
+    data() {
+        return {
+            songs: [],
+        }
+    },
+
+    created() {
+        this.load()
+    },
+
+    methods: {
+        load() {
+            axios
+                .get('http://localhost:8080/api/songs')
+                .then(response => {
+                    console.log(response)
+                    this.songs = response.data._embedded.songs
+                })
+        }
     },
 }
 </script>
